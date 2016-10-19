cd9e406bfa19aa5014373c0971732644f2c3fe9a
Coffeeteer <tgrieshiemer@gmail.com>
2016-10-19 12:24:44 -0400

    have the basic layout complete with server intact

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+'use strict';
+
 var express  = require('express');
 var app      = express();
+var http     = require('http');
+
+app.use(express.static(process.cwd() + 
+	'/public'));
 
 app.get('/', function(req, res) {
-  res.send('index', {
-    // user: req.user
-  });
+  res.send('index');
 });
 
 console.log('process.env.PORT', process.env.PORT);
@@ -12,3 +16,4 @@ var port = process.env.PORT || 3010;
 
 app.listen(port);
 console.log('The magic happens on port ' + port);
+
